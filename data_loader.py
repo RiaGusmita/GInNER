ff8113b4879919cead76e12e70ab1846acc9b7ff
Ria Hari Gusmita <mrs.langga@gmail.com>
2021-04-28 01:22:26 +0200

    new file:   data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 18:21:07 2021
+
+@author: asep
+"""
+
+import re
+from tqdm import tqdm
+import numpy as np
+import spacy
+parser = spacy.load("id_spacy")
+import torch
+#print(parser)
+
+classes = ["O", "B-PERSON", "I-PERSON", "B-LOC", "I-LOC", "B-ORG", "I-ORG"]
+_partial_word = '%pw'
+
+
+def readData(filename):
+    data = []
+    with open(filename, 'r') as f:
+        for line in tqdm(f):
+            values = line.split("\t")
+            #print(values[0], len(values[0].strip()))
+            if len(values[0].strip())>=1:
+                word = values[0]
+                label = values[1]
+                label = re.sub("\n", "", label)
+                label.strip()
+                items = [word, label]
+            else:
+                items = ["<EOS>", "<EOS>"]
+            data.append(items)
+    return data
+
+def getData():
+    train_filename = "datasets/train.bio"
+    dev_filename = "datasets/dev.bio"
+    test_filename = "datasets/test.bio"
+    train = readData(train_filename)
+    dev = readData(dev_filename)
+    test = readData(test_filename)
+    #print(test)
+    
+    return train, dev, test
+
+def createFullSentence(words):
+    sentence = ' '.join(words)
+    sentence = re.sub(r' (\'[a-zA-Z])', r'\1', sentence)
+    sentence = re.sub(r' \'([0-9])', r' \1', sentence)
+    sentence = re.sub(r' (,.)', r'\1', sentence)
+    sentence = re.sub(r' " (.*) " ', r' "\1" ', sentence)
+   
+    return sentence
+
+def getSentences(data):
+    words_and_label = []
+    sentences = []
+    for item in data:
+        #print(item)
+        if item[0] != "<EOS>":
+            word = item[0]
+            label = item[1]
+            words_and_label.append((word, label))
+        else:
+            sentences.append(words_and_label)
+            words_and_label = []
+    return sentences
+
+def get_data_from_sentences(sentences):
+    all_data = []
+    A = np.zeros((len(classes) + 1, len(classes) + 1))
+    total_tokens = 0
+    for sentence in tqdm(sentences):
+        word_data = []
+        class_data = []
+        class_text = []
+        words = []
+        prior_entity = len(classes)
+        #print('sentence', sentence)
+        for word, entity in sentence:
+            if word == _partial_word:
+                continue
+            words.append(word)
+            word_vector = get_word_vector(word)
+            vector = word_vector
+            class_vector = get_class_vector(entity)
+            entity_num = get_entity_num(entity)
+            word_data.append(vector)
+            class_data.append(entity_num)
+            class_text.append(entity)
+            A[prior_entity, entity_num] += 1
+            prior_entity = entity_num
+            total_tokens += 1
+        all_data.append((words, word_data, class_data, class_text))
+    return all_data
+
+def get_word_vector(word):
+    parsed = parser(word)
+    default_vector = parser('entity')[0].vector
+    try:
+        vector = parsed[0].vector
+        #print(vector.shape)
+        if vector_is_empty(vector):
+            vector = default_vector
+    except:
+        vector = default_vector
+    #print("vector", vector.shape)
+    return np.array(vector, dtype=np.float64) 
+
+def vector_is_empty(vector):
+    to_throw = 0
+    for item in vector:
+        if item == 0.0:
+            to_throw += 1
+    if to_throw == len(vector):
+        return True
+    return False  
+
+def get_class_vector(class_name):
+    vector = [0.] * (len(classes) + 1)
+    index = len(classes)
+    try:
+        index = classes.index(class_name)
+    except:
+        pass
+    vector[index] = 1.
+    return vector
+
+
+def get_entity_num(class_name):
+    entity_num = len(classes)
+    try:
+        entity_num = classes.index(class_name)
+    except:
+        pass
+    return entity_num
+
+def get_class_name(class_num):
+    #print(classes)
+    entity = classes[class_num]
+    return entity
+
+def create_graph_from_sentence_and_word_vectors(sentence, word_vectors):
+    if not isinstance(sentence, str):
+        raise TypeError("String must be an argument")
+    from igraph import Graph
+    from nl import SpacyTagger as Tagger, SpacyParser as Parser
+    from scipy.sparse import coo_matrix
+
+    sentence = re.sub(r'([a-zA-Z]+)-([a-zA-Z]+)', r' \1_\2 ', sentence)
+    
+    tagger = Tagger(sentence)
+    parser = Parser(tagger)
+
+    X = []
+    for i in range(len(word_vectors)):
+        X.append(word_vectors[i])
+    X = np.array(X)
+
+    nodes, edges, words, tags, types = parser.execute()
+    g = Graph(directed=True)
+    g.add_vertices(nodes)
+    g.add_edges(edges)
+    A = np.array(g.get_adjacency().data)
+    A = coo_matrix(A)
+
+    #print(X)
+    return A, torch.tensor(X)
+
+def get_words_embeddings_from_sentence(sentence):
+    sentence = re.sub(r'([a-zA-Z]+)-([a-zA-Z]+)', r' \1_\2 ', sentence)
+    tokens = parser(sentence)
+    return _get_word_vectors_from_tokens(tokens)
+
+
+def _get_word_vectors_from_tokens(tokens):
+    words = []
+    vectors = []
+    idx = []
+    for token in tokens:
+        word = token.orth_
+        #tag = token.tag_
+        words.append(word)
+        vectors.append(get_word_vector(word))
+        idx.append([token.idx, token.idx + len(token.orth_)])
+    return words, vectors, idx
---
+++ model_testing.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 15:26:54 2021
+
+@author: asep
+"""
+from model import GInNER
+import os.path as path
+import torch
+from data_loader import getSentences, get_data_from_sentences, createFullSentence, create_graph_from_sentence_and_word_vectors, get_class_name
+from tqdm import tqdm
+import torch
+from sklearn.metrics import recall_score, precision_score, f1_score
+import logging
+import random
+
+_logger = logging.getLogger(__name__)
+
+def model_testing(test_dataset, device, dropout, hidden_layer, nheads, word_embedding_dim=96):
+    #print(test_dataset)
+    sentences = getSentences(test_dataset)
+    #print("sentences", sentences)
+    #print('len sentences', len(sentences))
+    data = get_data_from_sentences(sentences)
+    ginner = GInNER(word_embedding_dim, device, dropout, hidden_layer, nheads)
+    checkpoint = torch.load(path.join("models", "checkpoint_epoch_{}.pt".format(49)))
+    ginner.load_state_dict(checkpoint["model_state_dict"])
+    print(ginner)
+    ginner.to(device)
+    #print('len data', len(data))
+    recall_scores = []
+    precision_scores = []
+    f1_scores = []
+    broken_sentences=0
+    for item in tqdm(data):
+        words = item[0]
+        labels = torch.LongTensor(item[2])
+        #labels = labels.to(device).type(torch.LongTensor)
+        word_embeddings = item[1]
+        sentence = createFullSentence(words)
+        try:           
+            A, X = create_graph_from_sentence_and_word_vectors(sentence, word_embeddings)
+            #print("A", A.shape[0])
+            #print("X", len(X))
+                        
+            output_tensor = ginner(X, A)
+            output_tensor = output_tensor
+            #print(output_tensor.shape)
+            logits_scores, logits_tags = torch.max(output_tensor, 1, keepdim=True)
+            logits_label = logits_tags.detach().cpu().numpy().tolist()
+            y_pred = [predict[0] for predict in logits_label]
+            y_true = labels.detach().cpu().numpy().tolist()
+            recall_scores.append(recall_score(y_true, y_pred, average='macro'))
+            precision_scores.append(precision_score(y_true, y_pred, average='macro'))
+            f1_scores.append(f1_score(y_true, y_pred, average='macro'))
+        except:
+            _logger.warning('Cannot process the following sentence: ' + sentence)
+            broken_sentences += 1
+            continue
+        
+    avg_recall = sum(recall_scores)/len(recall_scores)
+    avg_precision = sum(precision_scores)/len(precision_scores)
+    avg_fscores = sum(f1_scores)/len(f1_scores)
+    print("total sentences", len(sentences))
+    print("Broken sentences", broken_sentences)
+    print("precision {}, recall {}, f1 {}".format(avg_precision, avg_recall, avg_fscores))
+    evaluate_randomly(data, ginner)
+    
+def evaluate_randomly(data, model):
+    item = random.choice(data)
+    
+    words = item[0]
+    word_embeddings = item[1]
+    sentence = createFullSentence(words)
+    labels = item[2]
+    A, X = create_graph_from_sentence_and_word_vectors(sentence, word_embeddings)
+    output_tensor = model(X, A)
+    output_tensor = output_tensor
+    logits_scores, logits_tags = torch.max(output_tensor, 1, keepdim=True)
+    logits_label = logits_tags.detach().cpu().numpy().tolist()
+    y_pred = [predict[0] for predict in logits_label]
+    for i, word in enumerate(words):
+        print("word {} prediction {} True label {}".format(word, get_class_name(y_pred[i]), get_class_name(labels[i])))
+
+def test_ner(sentence):
+    
+    words = item[0]
+    word_embeddings = item[1]
+    sentence = createFullSentence(words)
+    labels = item[2]
+    A, X = create_graph_from_sentence_and_word_vectors(sentence, word_embeddings)
+    output_tensor = model(X, A)
+    output_tensor = output_tensor
+    logits_scores, logits_tags = torch.max(output_tensor, 1, keepdim=True)
+    logits_label = logits_tags.detach().cpu().numpy().tolist()
+    y_pred = [predict[0] for predict in logits_label]
+    for i, word in enumerate(words):
+        print("word {} prediction {} True label {}".format(word, get_class_name(y_pred[i]), get_class_name(labels[i])))
