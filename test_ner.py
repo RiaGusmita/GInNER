8c451be7bf651f00d8aea2fa3027f8a83e3f9a4b
Ria Hari Gusmita <mrs.langga@gmail.com>
2021-05-02 02:56:24 +0200

    modified:   main.py 	modified:   test_ner.py

--- test_ner.py
+++ test_ner.py
@@ -5,7 +5,7 @@ Or pipe the text from the command line: python test_ner.py < data/random_text.tx
 '''
 import torch
 import os.path as path
-from data_loader import get_words_embeddings_from_sentence, create_graph_from_sentence_and_word_vectors, get_class_name, get_words_embeddings_from_sentence_fasttext
+from data_loader import get_words_embeddings_from_sentence, create_graph_from_sentence_and_word_vectors, get_class_name, get_words_embeddings_from_sentence_fasttext, get_words_embeddings_from_sentence_indobert
 import argparse
 
 DEVICE = torch.device("cpu")
@@ -17,22 +17,33 @@ def _aggregate_sentence(args):
     return return_str
 
 
-def _get_entity_tuples_from_sentence(sentence, word_emb_model, word_embedding_dim, use_epoch):
+def _get_entity_tuples_from_sentence(sentence, word_emb_model, word_embedding_dim, ner_model):
     from model import GInNER
     import fasttext
     import fasttext.util
+    from transformers import BertTokenizer, AutoModel
+    
     word_emb=""
     if word_emb_model=="fasttext":
         word_emb = fasttext.load_model('cc.id.300.bin')
         word_emb = fasttext.util.reduce_model(word_emb, word_embedding_dim)
+    elif word_emb_model=="indobert":
+        tokenizer = BertTokenizer.from_pretrained("indobenchmark/indobert-base-p1")
+        model = AutoModel.from_pretrained("indobenchmark/indobert-base-p1")
+        word_emb = (tokenizer, model)
         
     ginner = GInNER(word_embedding_dim, DEVICE, dropout=0.0, hidden_layer=2, nheads=3)
-    checkpoint = torch.load(path.join("models", "checkpoint_epoch_{}.pt".format(use_epoch)))
+    checkpoint = torch.load(path.join("models", ner_model))
     ginner.load_state_dict(checkpoint["model_state_dict"])
     ginner.to(DEVICE)
     
     if word_emb_model=="fasttext":
         words, vectors = get_words_embeddings_from_sentence_fasttext(sentence, word_emb)
+    elif word_emb_model=="fasttext":
+        tokenizer = BertTokenizer.from_pretrained("indobenchmark/indobert-base-p1")
+        model = AutoModel.from_pretrained("indobenchmark/indobert-base-p1")
+        words, vectors = get_words_embeddings_from_sentence_fasttext(sentence, tokenizer, model)
+        
     else:
         words, vectors, idx = get_words_embeddings_from_sentence(sentence)
     
@@ -53,7 +64,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Indonesian NER')
     parser.add_argument("--word_emb_model", type=str, default="spacy", help="Word embedding models: spacy/fasttext")
     parser.add_argument("--word_emb_dim", type=int, default=96, help="Word embedding dimension")
-    parser.add_argument("--use_epoch", type=int, default=10, help="Which use best epoch")
+    parser.add_argument("--ner_model", type=str, default="final_model.pt", help="NER model")
     
     args = parser.parse_args()
     if os.isatty(0):
